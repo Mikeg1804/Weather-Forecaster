e60d64f1ec1dde25d70c50bd0799ccde10149209
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-03-10 18:00:22 -0800

    added API request

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4fb2c1c17b8775d8bd78a36c089c1827';
+
+var requestLocation = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=4fb2c1c17b8775d8bd78a36c089c1827'
