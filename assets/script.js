65d1f368a8cd72548f1b41ef1830e507eef7cd5a
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-03-11 13:13:41 -0800

    updated branch

--- assets/script.js
+++ assets/script.js
@@ -5,25 +5,30 @@ var textSearchEl = document.getElementById("citySearch")
 var requestUrl = 'api.openweathermap.org/data/2.5/forecast?&appid=4fb2c1c17b8775d8bd78a36c089c1827';
 // var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' +lat +'&lon=' + lon + '&appid=4fb2c1c17b8775d8bd78a36c089c1827';
 
+
+
+let citySearch = searchEl.addEventListener("click",function(){
+  console.log(textSearchEl.value)
+ return textSearchEl.value
+
+    })
+
+console.log(citySearch)    
+
 var requestLocation = 'http://api.openweathermap.org/geo/1.0/direct?q=' +citySearch+ '&limit=4fb2c1c17b8775d8bd78a36c089c1827';
 
+console.log(requestLocation)
 
 function getApi(requestLocation) {
     fetch(requestLocation) 
       .then(function (response) {
-        console.log(response);
+        console.log(response)
         return response.json();
     });
   }
 
 
-let citySearch = searchEl.addEventListener("click",function(){
-console.log(textSearchEl.value)
-  // var user = nameInputEl.value
-  // var highScoreList = JSON.parse(localStorage.getItem("highScore")) || []
-  // highScoreList.push({
-  //   user:user,score:secondsLeft
-  })
+
 
 // rootEl.append(html)
 // //Event Bubbling
