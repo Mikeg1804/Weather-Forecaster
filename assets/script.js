e97c2ef3eb96c30a9dee0d406222f784d4b69e4c
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-03-17 18:16:12 -0700

    fix column issues

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,15 @@
 var searchEl = document.getElementById("searchBtn")
 var textSearchEl = document.getElementById("citySearch")
 var forecastTemparature = document.getElementById("fivedayContainer")
+var futureDate = document.getElementById("futureDateContainer")
 var currentDay = document.querySelector('#currentDay')
 var currentTemperature = document.querySelector('#currentTemperature')
+var currentDate = document.querySelector('#currentDate')
+
 
 //var requestUrl = 'api.openweathermap.org/data/2.5/forecast?&appid=4fb2c1c17b8775d8bd78a36c089c1827';
  function fiveDayDisplay(lat,lon) {
-  var stringer2 = 'https://api.openweathermap.org/data/2.5/forecast?lat=' +lat +'&lon=' + lon + '&appid=4fb2c1c17b8775d8bd78a36c089c1827';
+  var stringer2 = 'https://api.openweathermap.org/data/2.5/forecast?lat=' +lat +'&lon=' + lon + '&units=imperial&appid=4fb2c1c17b8775d8bd78a36c089c1827';
   fetch(stringer2)
   .then((response) => response.json())
   .then((data) => {
@@ -17,30 +20,13 @@ var currentTemperature = document.querySelector('#currentTemperature')
       console.log(data.list[i])
       let oneDayCard = document.createElement ("div")
       oneDayCard.innerHTML= data.list[i].main.temp
-      forecastTemparature.append(oneDayCard)
+      forecastTemparature.append(oneDayCard);
+      // need to fix code below.
+      futureDate.append(document.createElement('div').innerHTML=data.list[i].dt_txt);
      } 
-     else(
-     currentTemperature.append(document.createElement ('div').innerHTML=data.list[i].main.temp)
-     )
     }
   });
  } 
-
-
- 
-// var citySearch;
-
-// searchEl.addEventListener("click",function(){
-//   citySearch = textSearchEl.value;
-//   // console.log(citySearch)
-//     });
-
-// // console.log(citySearch)    
-
-// function requestLocation(citySearch) {
-//   var requestLocation = 'http://api.openweathermap.org/geo/1.0/direct?q=' + citySearch + '&limit=4fb2c1c17b8775d8bd78a36c089c1827';
-//   console.log(requestLocation);
-//   }
 
 //function to fetch the result from the API request.
 function getApi(stringer) {
@@ -48,12 +34,21 @@ function getApi(stringer) {
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
+    currentTemperature.append(document.createElement ('div').innerHTML=data.main.temp)
     fiveDayDisplay(data.coord.lat,data.coord.lon)
+  //wrong date is being shown
+    currentDate.append(document.createElement ('div').innerHTML=dayjs())
+    35
+
+    // wrond date is being shown
+    const date = new Date();
+    const today = date.getDate()
+    console.log(today.toDateString())
   })
 }
 // function to put city inside of API
   function locationCity (citySearch) {
-    requestLocation = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearch + '&appid=4fb2c1c17b8775d8bd78a36c089c1827';
+    requestLocation = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearch + '&units=imperial&appid=4fb2c1c17b8775d8bd78a36c089c1827';
     console.log(requestLocation);
     getApi(requestLocation)
   }
@@ -65,19 +60,3 @@ function getApi(stringer) {
 
 
 
-  // function getApi(requestUrl) {
-  //   fetch(requestUrl) 
-  //     .then(function (response) {
-  //       console.log(response)
-  //       return response.json()
-  //   });
-  // }
-
-
-// rootEl.append(html)
-// //Event Bubbling
-// searchEl.on("click","searchBtn",function(){
-//   var userDayPlan = $(this).siblings("textarea").val()
-//   var id = $(this).parent().attr("id")
-//   localStorage.setItem(id,userDayPlan)
-// })
